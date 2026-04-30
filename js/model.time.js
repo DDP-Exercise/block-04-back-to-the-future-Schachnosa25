6662bddf1bbea8c1bdb2206a8aea924675b3a7e9
Schachnosa25 <s2510892032@fhooe.at>
2026-04-30 21:48:17 +0200

    morty come back

--- js/model.time.js
+++ js/model.time.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// data and logic
+
+export let clockModel = {
+    getTime: function() {
+        const now = new Date();
+
+        return {
+            seconds: now.getSeconds(),
+            minutes: now.getMinutes(),
+            hours: now.getHours()
+        };
+    }
+};
